b60860e9aa7c254aab859932b3936099aeaa0eea
Raymund Lew <raymund.lew@gmail.com>
2015-07-05 16:33:16 -0700

    Get a first world problems from Twitter and display a random one

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var twitter = require('twitter-app-api');
+var keys = require('./keys.js').keys;
+
+var twit = new twitter(keys.twitterConsumerKey, keys.twitterConsumerSecret);
+
+var resultingTweets = [];
+var fwpTweet = '';
+
 
 app.get('/', function(req, res) {
-  res.send('hello world');
+  twit.authenticate().then(function() {
+    twit.search({
+      q: 'firstworldproblems',
+      lang: 'en'
+    }).then(function(tweets) {
+      tweets.statuses.forEach(function(twit) {
+        resultingTweets.push(twit.text);
+      });
+      fwpTweet = resultingTweets[Math.floor(Math.random() * resultingTweets.length)];
+      res.send(fwpTweet);
+    });
+  });
 });
 
 app.listen(3000);
